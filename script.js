10b924a623a7d52b60b5638eca270797140cca75
Nxus <150361945+DumbNoxx@users.noreply.github.com>
2025-04-08 15:09:19 -0400

    terminated

--- script.js
+++ script.js
@@ -72,11 +72,7 @@ const locations = [
   {
     name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
-    "button functions": [
-      (button1.onclick = goStore),
-      (button2.onclick = goCave),
-      (button3.onclick = fightDragon),
-    ],
+    "button functions": [goStore, goCave, fightDragon],
     text: 'You are in the town square. You see a sign that says "Store".',
   },
   {
@@ -86,11 +82,7 @@ const locations = [
       "Buy weapon (30 gold)",
       "Go to town square",
     ],
-    "button functions": [
-      (button1.onclick = buyHealth),
-      (button2.onclick = buyWeapon),
-      (button3.onclick = goTown),
-    ],
+    "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store.",
   },
   {
@@ -106,11 +98,7 @@ const locations = [
   {
     name: "fight",
     "button text": ["Attack", "Dodge", "Run"],
-    "button functions": [
-      (button1.onclick = attack),
-      (button2.onclick = dodge),
-      (button3.onclick = goTown),
-    ],
+    "button functions": [attack, dodge, goTown],
     text: "You are fighting a monster.",
   },
   {
@@ -120,12 +108,26 @@ const locations = [
       "Go to town square",
       "Go to town square",
     ],
-    "button functions": [
-      (button1.onclick = goTown),
-      (button2.onclick = goTown),
-      (button3.onclick = goTown),
-    ],
+    "button functions": [goTown, goTown, easterEgg],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
+  },
+  {
+    name: "lose",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You die. &#x2620;",
+  },
+  {
+    name: "win",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;",
+  },
+  {
+    name: "easter egg",
+    "button text": ["2", "8", "Go to town square?"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!",
   },
 ];
 
@@ -142,7 +144,7 @@ function update(location) {
   button1.onclick = location["button functions"][0];
   button2.onclick = location["button functions"][1];
   button3.onclick = location["button functions"][2];
-  text.innerText = location.text;
+  text.innerHTML = location.text;
 }
 
 function goTown() {
@@ -233,17 +235,40 @@ function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText +=
     " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-  health -= monsters[fighting].level;
-  monsterHealth -=
-    weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  health -= getMonsterAttackValue(monsters[fighting].level);
+  if (isMonsterHit()) {
+    monsterHealth -=
+      weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  } else {
+    text.innerText += " You miss.";
+  }
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
     lose();
   } else if (monsterHealth <= 0) {
-    defeatMonster();
+    if (fighting === 2) {
+      winGame();
+    } else {
+      defeatMonster();
+    }
+  }
+  if (Math.random() <= 0.1 && inventory.length !== 1) {
+    text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentWeaponIndex--;
   }
 }
+
+function getMonsterAttackValue(level) {
+  const hit = level * 5 - Math.floor(Math.random() * xp);
+  console.log(hit);
+  return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit() {
+  return Math.random() > 0.2 || health < 20;
+}
+
 function dodge() {
   text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
@@ -257,4 +282,60 @@ function defeatMonster() {
   xpText.innerText = xp;
   update(locations[4]);
 }
-function lose() {}
+
+function lose() {
+  update(locations[5]);
+}
+
+function winGame() {
+  update(locations[6]);
+}
+
+function restart() {
+  gold = 50;
+  xp = 0;
+  health = 100;
+  inventory = ["stick"];
+  currentWeaponIndex = 0;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  healthText.innerText = health;
+  goTown();
+}
+
+// Easter Egg
+
+function easterEgg() {
+  update(locations[7]);
+}
+
+function pick(guess) {
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; i < 10; i++) {
+    text.innerText = numbers[i] + "\n";
+  }
+  if (numbers.includes(guess)) {
+    text.innerText += "Right! You win 20 gold!";
+    gold += 20;
+    goldText.innerText = gold;
+  } else {
+    text.innerText += "Wrong! You lose 10 health!";
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
+  }
+}
+
+function pickTwo(guess) {
+  pick(2);
+}
+
+function pickEight(guess) {
+  pick(8);
+}
